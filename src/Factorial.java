7ed2df516ea730055c0ef2ab1736f23ac9b3ae0f
kmkim0713 <kmkimworkwork@gmail.com>
2024-03-06 20:04:09 +0900

    facorial 작성

---
+++ src/Factorial.java
@@ -0,0 +1,14 @@
+import java.util.EmptyStackException;
+
+public class Factorial {
+
+    public static int recur(int num){
+        if (num == 1) return num;
+        return num * recur(num - 1);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(recur(5));
+    }
+}
