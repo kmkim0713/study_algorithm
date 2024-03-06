c5f8d7a1bd5ad898dc4f41866ddabd24b384d58c
KyungMinKim <kmkimworkwork@gmail.com>
2024-03-06 12:18:00 +0900

    이진검색

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -4,22 +4,49 @@ public class BinarySearch {
 
     public static void main(String[] args) {
 
-        int input = 100;
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        arr.add(1);
+        arr.add(5);
+        arr.add(8);
+        arr.add(12);
+        arr.add(18);
+        arr.add(23);
+        arr.add(26);
+        arr.add(30);
+        arr.add(33);
+        arr.add(36);
+        arr.add(38);
+        arr.add(39);
+        arr.add(40);
 
-        // input까지의 숫자중 작은 소수 모두 나열
+        int divisionIndex;
+        int startIndex = 0;
+        int endIndex = arr.size()-1;
 
-        ArrayList<Integer> result = new ArrayList<>();
+        int targetNum = 26;
+        int targetIndex = -1;
 
-        for (int i=2; i<=input; i++){
-            if (i % 2 != 0 && i % 3 != 0){
-                result.add(i);
-                System.out.println(i+"");
+        while(startIndex <= endIndex){
+
+            divisionIndex = (startIndex + endIndex)/2;
+
+            if(targetNum == arr.get(divisionIndex)) {
+                targetIndex = divisionIndex;
+                break;
+            }
+            else if(targetNum < arr.get(divisionIndex)){
+                endIndex = divisionIndex - 1;
+            }
+            else if(targetNum > arr.get(divisionIndex)){
+                startIndex = divisionIndex + 1;
             }
         }
 
-
-
+        if (targetIndex == -1){
+            System.out.println("찾는 값이 없음");
+        } else {
+            System.out.println("값의 인덱스: "+targetIndex);
+        }
     }
-
 
 }
---
+++ src/FindPrime.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class FindPrime {
+
+    public static void main(String[] args) {
+
+        int input = 100;
+
+        // input까지의 숫자중 작은 소수 모두 나열
+
+        ArrayList<Integer> result = new ArrayList<>();
+
+        for (int i=2; i<=input; i++){
+            if (i % 2 != 0 && i % 3 != 0){
+                result.add(i);
+                System.out.println(i+"");
+            }
+        }
+
+
+
+    }
+
+
+}
