0ed16d1f06619df5d9374269abcb3dc8a086dc4e
kmkim0713 <kmkimworkwork@gmail.com>
2024-03-10 23:25:40 +0900

    퀵 정렬 피벗을 중앙 값으로 정한 경우

--- src/Sort/QuickSortLeftPivot.java
+++ src/Sort/QuickSortLeftPivot.java
@@ -2,7 +2,7 @@ package Sort;
 
 import java.io.IOException;
 
-public class QuickSort {
+public class QuickSortLeftPivot {
     // 퀵 정렬 (좌측 끝값을 피벗으로)
     // https://erinh.tistory.com/entry/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%ED%80%B5-%EC%A0%95%EB%A0%AC-Quick-sort-%EC%9E%90%EB%B0%94-Java
 
@@ -28,30 +28,30 @@ public class QuickSort {
 
         // 가장 왼쪽의 값을 pivot으로 지정. 비교검사는 pivot + 1 부터 시작.
         int pivot = start;
-        int lo = start + 1;
-        int hi = end;
+        int left = start + 1;
+        int right = end;
 
 
-        while (lo <= hi) {
+        while (left <= right) {
 
-            while (lo <= end && arr[lo] <= arr[pivot]) lo++; // 배열의 끝까지 검사. pivot과 같거나 더 작은지 검사하면서 인덱스 증가.
+            while (left <= end && arr[left] <= arr[pivot]) left++; // 배열의 끝까지 검사. pivot 같거나 더 작은지 검사하면서 인덱스 증가.
 
-            while (hi > start && arr[hi] >= arr[pivot]) hi--; // pivot보다 작은 인덱스로 갈 순 없음. pivot보다 큰지 검사하면서 인덱스 감소.
+            while (right > start && arr[right] >= arr[pivot]) right--; // pivot 작은 인덱스로 갈 순 없음. pivot보다 큰지 검사하면서 인덱스 감소.
 
-            if (lo > hi) { // 엇갈렸을 때는 오른쪽에서 시작한 hi와 피벗을 교체. hi가 있던 위치는 정렬위치가 확정
-                swap(arr, hi, pivot);
-            } else { // 엇갈리지 않으면 서로 피벗보다 크거나 작아서 그런것이니 lo, hi 값 교체
-                swap(arr, lo, hi);
+            if (left > right) {
+                swap(arr, right, pivot); // 엇갈렸을 때는 오른쪽에서 시작한 hi와 피벗을 교체. hi가 있던 위치는 정렬위치가 확정
+            } else {
+                swap(arr, left, right); // 엇갈리지 않으면 서로 피벗보다 크거나 작아서 그런것이니 left, right 값 교체
             }
         }
 
-        quickSort(arr, start, hi - 1);
-        quickSort(arr, hi + 1, end);
+        quickSort(arr, start, right - 1);
+        quickSort(arr, right + 1, end);
 
     }
 
 
-    private static void swap(int[] arr, int a, int b) {
+    public static void swap(int[] arr, int a, int b) {
         int temp = arr[a];
         arr[a] = arr[b];
         arr[b] = temp;
---
+++ src/Sort/QuickSortMiddlePivot.java
@@ -0,0 +1,77 @@
+package Sort;
+
+import java.io.IOException;
+
+public class QuickSortMiddlePivot {
+    // 퀵 정렬 (좌측 끝값을 피벗으로)
+    // https://erinh.tistory.com/entry/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%ED%80%B5-%EC%A0%95%EB%A0%AC-Quick-sort-%EC%9E%90%EB%B0%94-Java
+
+    public static void main(String[] args) throws IOException {
+
+        int[] arr = {35, 17, 7, 22, 86, 49, 10};
+        quickSort(arr);
+
+        printArr("정렬 후 ", arr, -1, -1);
+
+    }
+
+    public static void quickSort(int[] arr) {
+
+        quickSort(arr, 0, arr.length - 1);
+
+    }
+
+    public static void quickSort(int[] arr, int start, int end) {
+
+        if (start >= end) return;
+
+        int left = start;
+        int right = end;
+        int pivot = partition(arr, left, right);
+
+        quickSort(arr, left, pivot - 1);
+        quickSort(arr, pivot, right);
+
+
+    }
+
+    public static int partition(int[] arr, int left, int right) {
+        int pivot = arr[(left + right) / 2];
+
+        while(left <= right){
+
+            while(arr[left] < pivot) left++;
+
+            while(arr[right] > pivot) right--;
+
+            if (left <= right){ // 엇갈리지 않으면 swap. while문 조건과 동일.
+                swap(arr, left, right);
+                left ++;
+                right --;
+            }
+        }
+
+        // 엇갈리게 되면 while문 종료
+        return left;
+    }
+
+    public static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
+
+    public static void printArr(String msg, int[] arr, int a, int b) {
+        System.out.print(msg + "\t\t");
+        for (int i = 0; i < arr.length; i++) {
+            if (i == a || i == b) {
+                System.out.printf("[%d] ", arr[i]);
+                continue;
+            }
+            System.out.printf("%d ", arr[i]);
+        }
+        System.out.println();
+
+    }
+}
