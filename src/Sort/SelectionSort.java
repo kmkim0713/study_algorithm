ee11b868367330d808f5519020e0358b326c3d24
kyungminkim <kmkimworkwork@gmail.com>
2024-03-12 11:23:53 +0900

    선택정렬 다시 작성

--- src/Sort/SelectionSort.java
+++ src/Sort/SelectionSort.java
@@ -3,53 +3,53 @@ package Sort;
 import java.io.IOException;
 
 public class SelectionSort {
+    // 선택 정렬
+    // 가장 작은 값을 제일 앞으로 보내기
 
-    // 선택정렬
+    public static void main(String[] args) {
 
-    public static void main(String[] args) throws IOException {
-
-        int[] arr = {15, 17, 52, 10, 3, 49, 33};
-        int n = arr.length;
-
+        int[] array = {15, 17, 52, 10, 3, 49, 33};
+        int n = array.length;
         int start = 0;
 
-        while (start < n - 1) {
+        while (start < n) {
 
-            printArr("[pass 수행 전 ]", arr, -1, -1);
-            int targetIndex = start;
-            int targetValue = arr[start];
-            for (int j = n - 1; j > start; j--) {
-                if (targetValue > arr[j]) {
-                    targetValue = arr[j];
-                    targetIndex = j;
+            int min = array[start];
+            int swapIndex = start;
+
+            for (int j = n - 1; j >= start; j--) {
+                if (min > array[j]) {
+                    min = array[j];
+                    swapIndex = j;
                 }
             }
-            printArr("[가장 작은 숫자]", arr, targetIndex, -1);
-            swap(arr, start, targetIndex);
+
+            swap(array, start, swapIndex);
+            printarray("정렬 후... ", array, start, swapIndex);
             start++;
-            System.out.println();
         }
+
+        printarray("[최종 출력]", array, -1, -1);
+
     }
 
-
-    public static void swap(int[] arr, int a, int b) {
+    public static void swap(int[] array, int a, int b) {
 
         int temp = 0;
-        temp = arr[a];
-        arr[a] = arr[b];
-        arr[b] = temp;
-        printArr("[swap 후 ]\t", arr, a, b);
+        temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
 
     }
 
-    public static void printArr(String msg, int[] arr, int a, int b) {
+    public static void printarray(String msg, int[] array, int a, int b) {
         System.out.printf(msg + "\t\t");
-        for (int i = 0; i < arr.length; i++) {
-            if (i == a || i == b) {
-                System.out.printf("[%d] ", arr[i]);
+        for (int i = 0; i < array.length; i++) {
+            if (i == a) {
+                System.out.printf("[%d] ", array[i]);
                 continue;
             }
-            System.out.printf("%d ", arr[i]);
+            System.out.printf("%d ", array[i]);
         }
         System.out.println();
 
