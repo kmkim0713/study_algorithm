78352620b8786325a5d38a3c4f101e951e37e9af
kmkim0713 <kmkimworkwork@gmail.com>
2024-03-12 22:55:19 +0900

    힙 정렬 완성

--- src/Sort/HeapSort.java
+++ src/Sort/HeapSort.java
@@ -12,22 +12,21 @@ public class HeapSort {
 
         // heapify
         heapify(arr, arr.length);
-//        printArr("최대 힙 구조 배열 ", arr, -1, -1);
 
+        // heap sort
         for (int i = arr.length - 1; i > 0; i--) {
-            swap(arr, 0, i);
-//            printArr("최대값을 맨 뒤와 바꾼 후 배열 ", arr, -1, -1);
-            heapify(arr,i);
-
+            swap(arr, 0, i); // 루트 노드를 마지막 요소와 바꾼다.
+            heapify(arr, i); // 바뀐 구조를 기준으로 최대힙구조를 만든다.
         }
 
         printArr("정렬 후 ", arr, -1, -1);
 
     }
 
-
-
     // 최대 힙 구조 만들기
+    // 첫번째 자식(i=1)부터 시작
+    // 자식으로 취급되는 i번째 노드의 인덱스를 child에 저장. child 인덱스의 부모 노드를 확인하고 부모노드보다 크면 swap
+    // child 인덱스를 기준으로 부모 노드를 찾기위해 다시 반복문 수행. child 인덱스 값이 루트 인덱스가 되기 전까지 반복
     public static void heapify(int[] a, int idx) {
 
         for (int i = 1; i < idx; i++) { // i = 1 첫번째 자식노드부터. i가 0이면 루트 노드이다.
@@ -42,13 +41,12 @@ public class HeapSort {
                     printFlag = true;
                 }
 
-                child = parent; // 바뀐 부모노드를 자식노드로 바꿔준다.
                 if (printFlag) {
-                    printArr("[SWAP] child: "+child+" "+"parent: "+parent, a, -1, -1);
-                } else{
-                    printArr("child: "+child+" "+"parent: "+parent+"\t\t", a, -1, -1);
+                    printArr("[SWAP] i값: " + i + " child: " + child + " parent: " + parent + "\t", a, -1, -1);
+                } else {
+                    printArr("i값: " + i + " child: " + child + " parent: " + parent + "\t\t", a, -1, -1);
                 }
-
+                child = parent; // 바뀐 부모노드를 자식노드로 바꿔준다.
             }
         }
     }
