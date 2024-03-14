3191755d282b71691c609470af5dde61a587f6a2
kmkim0713 <kmkimworkwork@gmail.com>
2024-03-14 19:52:13 +0900

    subString 추가

---
+++ src/String/SubString.java
@@ -0,0 +1,15 @@
+package String;
+
+import java.io.IOException;
+
+public class SubString {
+    // 힙 정렬
+
+    public static void main(String[] args) throws IOException {
+
+        String str = "aaa김경민bbb";
+        System.out.println(str.substring(3,6));
+
+    }
+
+}
