ebf4baf517d8d93c68cb131b2ad28851b37f14fb
kmkim0713 <kmkimworkwork@gmail.com>
2024-03-16 22:55:56 +0900

    01-2 시간 복잡도 활용하기

---
+++ src/AlgorithmCodingTest/NumberSort2750/Main.java
@@ -0,0 +1,60 @@
+package AlgorithmCodingTest.NumberSort2750;
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        quickSort(arr, 0, arr.length - 1);
+
+        for(int temp : arr){
+            System.out.print(temp+" ");
+        }
+
+    }
+
+    public static void quickSort(int[] arr, int start, int end) {
+
+        if (start >= end) return;
+
+        int i = start + 1;
+        int j = end;
+
+        int pivot = start;
+        int pivotValue = arr[pivot];
+
+        while (i <= j) {
+
+            while (pivotValue >= arr[i]) i++;
+
+            while (pivotValue <= arr[j] && j > start) j--;
+
+            if (i > j) {
+                swap(arr, pivot, j);
+            } else {
+                swap(arr, i, j);
+            }
+        }
+
+        quickSort(arr, start, j - 1);
+        quickSort(arr, j + 1, end);
+
+    }
+
+    public static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = b;
+        arr[b] = temp;
+    }
+
+}
